5eb4f2d30f21013f515debcc65f5812d2216a810
Cameron Wong <hgneng@gmail.com>
2024-09-05 12:15:11 +0800

    finish cantonese phonemizer

--- TTS/tts/utils/text/cantonese/phonemizer.py
+++ TTS/tts/utils/text/cantonese/phonemizer.py
@@ -1,37 +1,37 @@
 from typing import List
 
-import jieba
-import pypinyin
+import re
+import pycantonese
 
-from .pinyinToPhonemes import PINYIN_DICT
+from .jyutpingToPhonemes import JYUTPING_DICT
 
+# @deprecated: no reference
+def _cantonese_character_to_jyutping(text: str) -> List[str]:
+    jyutpings = pycantonese.characters_to_jyutping(text)
+    ret = []
+    for word in jyutpings:
+        jyutpingWord = word[1]
+        ret.extend(re.findall(r'[a-zA-Z]+[0-9]+', jyutpingWord))
 
-def _chinese_character_to_pinyin(text: str) -> List[str]:
-    pinyins = pypinyin.pinyin(text, style=pypinyin.Style.TONE3, heteronym=False, neutral_tone_with_five=True)
-    pinyins_flat_list = [item for sublist in pinyins for item in sublist]
-    return pinyins_flat_list
+    return ret
 
-
-def _chinese_pinyin_to_phoneme(pinyin: str) -> str:
+# @deprecated: no reference
+def _cantonese_jyutping_to_phoneme(pinyin: str) -> str:
     segment = pinyin[:-1]
     tone = pinyin[-1]
-    phoneme = PINYIN_DICT.get(segment, [""])[0]
+    phoneme = JYUTPING_DICT.get(segment, [""])[0]
     return phoneme + tone
 
+def cantonese_text_to_phonemes(text: str, seperator: str = "|") -> str:
+    jyutpings = pycantonese.characters_to_jyutping(text)
+    tokens = []
+    for word in jyutpings:
+        jyutpingWord = word[1]
+        if jyutpingWord == None:
+            tokens.append('')
+        else:
+            tokens.extend(re.findall(r'[a-zA-Z]+[0-9]+', jyutpingWord))
 
-def chinese_text_to_phonemes(text: str, seperator: str = "|") -> str:
-    tokenized_text = jieba.cut(text, HMM=False)
-    tokenized_text = " ".join(tokenized_text)
-    pinyined_text: List[str] = _chinese_character_to_pinyin(tokenized_text)
+        tokens.append('')
 
-    results: List[str] = []
-
-    for token in pinyined_text:
-        if token[-1] in "12345":  # TODO transform to is_pinyin()
-            pinyin_phonemes = _chinese_pinyin_to_phoneme(token)
-
-            results += list(pinyin_phonemes)
-        else:  # is ponctuation or other
-            results += list(token)
-
-    return seperator.join(results)
+    return seperator.join(tokens)
---
+++ TTS/tts/utils/text/phonemizers/yue_cn_phonemizer.py
@@ -0,0 +1,61 @@
+from typing import Dict
+import sys
+sys.path.append('/home/hgneng/code/hgneng/TTS')
+from TTS.tts.utils.text.cantonese.phonemizer import cantonese_text_to_phonemes
+from TTS.tts.utils.text.phonemizers.base import BasePhonemizer
+
+_DEF_ZH_PUNCS = "、.,[]()?!〽~『』「」【】"
+
+
+class YUE_CN_Phonemizer(BasePhonemizer):
+    """🐸TTS Yue-Cn phonemizer using functions in `TTS.tts.utils.text.cantonese.phonemizer`
+
+    Args:
+        punctuations (str):
+            Set of characters to be treated as punctuation. Defaults to `_DEF_ZH_PUNCS`.
+
+        keep_puncs (bool):
+            If True, keep the punctuations after phonemization. Defaults to False.
+
+    Example ::
+
+        "这是，样本中文。" -> `d|ʒ|ø|4| |ʂ|ʏ|4| |，| |i|ɑ|ŋ|4|b|œ|n|3| |d|ʒ|o|ŋ|1|w|œ|n|2| |。`
+    """
+
+    language = "yue-cn" # what about yue-hk?
+
+    def __init__(self, punctuations=_DEF_ZH_PUNCS, keep_puncs=False, **kwargs):  # pylint: disable=unused-argument
+        super().__init__(self.language, punctuations=punctuations, keep_puncs=keep_puncs)
+
+    @staticmethod
+    def name():
+        return "yue_cn_phonemizer"
+
+    @staticmethod
+    def phonemize_yue_cn(text: str, separator: str = "|") -> str:
+        ph = cantonese_text_to_phonemes(text, separator)
+        return ph
+
+    def _phonemize(self, text, separator):
+        return self.phonemize_yue_cn(text, separator)
+
+    @staticmethod
+    def supported_languages() -> Dict:
+        return {"yue-cn": "Cantonese"}
+
+    def version(self) -> str:
+        return "0.0.1"
+
+    def is_available(self) -> bool:
+        return True
+
+
+if __name__ == "__main__":
+    text = "这是，样本中文。"
+    e = YUE_CN_Phonemizer()
+    print(e.supported_languages())
+    print(e.version())
+    print(e.language)
+    print(e.name())
+    print(e.is_available())
+    print("`" + e.phonemize(text) + "`")
