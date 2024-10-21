9aae3c47945f92aff37b6374e049375211d9fba9
Cameron Wong <hgneng@gmail.com>
2024-10-21 11:46:10 +0800

    remove deubg code

--- TTS/tts/utils/visual.py
+++ TTS/tts/utils/visual.py
@@ -9,10 +9,10 @@ matplotlib.use("Agg")
 
 
 def plot_alignment(alignment, info=None, fig_size=(16, 10), title=None, output_fig=False, plot_log=False):
-    print('[cameron debug] plot_alignment: ', alignment)
+    #print('[cameron debug] plot_alignment: ', alignment)
     if isinstance(alignment, torch.Tensor):
         alignment_ = alignment.detach().cpu().numpy().squeeze(axis=0)
-        print('[cameron debug] alignment.detach().cpu().numpy().squeeze(): ', alignment_)
+        #print('[cameron debug] alignment.detach().cpu().numpy().squeeze(): ', alignment_)
     else:
         alignment_ = alignment
     alignment_ = alignment_.astype(np.float32) if alignment_.dtype == np.float16 else alignment_
