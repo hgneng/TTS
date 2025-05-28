1bb0b0f34b354ce7ce40ca836ec7834f016197d2
Cameron Wong <hgneng@gmail.com>
2025-05-28 11:08:53 +0800

    change output sample rate

--- recipes/mdcc/xtts_v2/train_cantonese.py
+++ recipes/mdcc/xtts_v2/train_cantonese.py
@@ -99,7 +99,7 @@ def main():
         gpt_use_perceiver_resampler=True,
     )
     # define audio config
-    audio_config = XttsAudioConfig(sample_rate=16000, dvae_sample_rate=22050, output_sample_rate=22050)
+    audio_config = XttsAudioConfig(sample_rate=16000, dvae_sample_rate=22050, output_sample_rate=24000)
     # training parameters config
     config = GPTTrainerConfig(
         output_path=OUT_PATH,
